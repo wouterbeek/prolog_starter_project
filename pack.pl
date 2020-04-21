ea98f9921d4b84c84a8b2a660b846e588ca720ab
Wouter Beek <wouter@triply.cc>
2020-04-21 19:39:21 +0200

    FIXED: Package naming must be consistent.

--- pack.pl
+++ pack.pl
@@ -2,7 +2,7 @@ author('Wouter Beek', 'wouter@triply.cc').
 download('https://github.com/wouterbeek/test/releases/*.zip').
 home('https://github.com/wouterbeek/test').
 maintainer('Wouter Beek', 'wouter@triply.cc').
-name(test).
+name(prolog_starter_project).
 packager('Wouter Beek', 'wouter@triply.cc').
 title('Test package.').
-version('1.0.0').
+version('1.0.1').
