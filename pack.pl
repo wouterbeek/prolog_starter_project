ebc83e1b4a57460f5942168612351ca19ca00cb2
Wouter Beek <wouter@triply.cc>
2020-04-21 20:04:02 +0200

    FIXED: Keep various metadata properties consistent.

--- pack.pl
+++ pack.pl
@@ -1,8 +1,8 @@
 author('Wouter Beek', 'wouter@triply.cc').
-download('https://github.com/wouterbeek/test/releases/*.zip').
-home('https://github.com/wouterbeek/test').
+download('https://github.com/wouterbeek/prolog_starter_project/releases/*.zip').
+home('https://github.com/wouterbeek/prolog_starter_project').
 maintainer('Wouter Beek', 'wouter@triply.cc').
 name(prolog_starter_project).
 packager('Wouter Beek', 'wouter@triply.cc').
-title('Prolog Starter Package').
-version('1.0.2').
+title('Prolog Starter Project').
+version('1.0.3').
